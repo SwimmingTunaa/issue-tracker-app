e7901decebe3eac387b847ec23205902579a2e4e
tvu100 <tuanvu@live.com.au>
2022-03-17 21:30:35 +1100

    Finished Login css and added local server

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,23 +1,45 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Login.scss';
 
-function Login()
+function Login(setToken)
 {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    async function loginUser(credentials)
+    {
+        return fetch('http://localhost:8080/login', {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(credentials)
+        }).then(data => data.json())
+    }
+
     return (
         <section className='login-wrapper'>
             <div className='login'>
-                <h1>Please Log In</h1>
+                <h1>Issue Tracker</h1>
                 <form >
                     <label>
-                        <p>Username</p>
-                        <input type="text" />
+                        <i class="fa-solid fa-user"></i>
+                        <input
+                            type="text"
+                            placeholder='Username'
+                            onChange={e => setUsername(e.target.value)}
+                        />
                     </label>
                     <label>
-                        <p>Password</p>
-                        <input type="password" />
+                        <i class="fa-solid fa-lock"></i>
+                        <input
+                            type="password"
+                            placeholder='Password'
+                            onChange={e => setPassword(e.target.value)}
+                        />
                     </label>
                     <div>
-                        <button type="submit">Log In</button>
+                        <button className='accent-button' type="submit">Log In</button>
                     </div>
                 </form>
             </div>
