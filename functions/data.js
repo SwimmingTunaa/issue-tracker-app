a02abf29cf73724045c7bd5683a4121bdfff5e06
tvu100 <tuanvu@live.com.au>
2022-03-22 23:27:22 +1100

    added json data

---
+++ functions/data.js
@@ -0,0 +1,14 @@
+const data = require('./loginData.json')
+const headers = {
+  'content-type': 'application/json',
+  'Access-Control-Allow-Origin': '*'
+}
+
+exports.handler = async () =>
+{
+    return {
+        body: JSON.stringify(data),
+        statusCode: 200,
+        headers: headers
+    }
+}
