44d610f906d81ad76638bd63acadd095cdbff95b
tvu100 <tuanvu@live.com.au>
2022-03-23 23:46:41 +1100

    working on nav

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,8 +1,8 @@
 import useToken from "./useToken";
 import Nav from "../Nav/Nav";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Dashboard from "../Dashboard/Dashboard";
-import Bugs from "../Bugs/Bugs";
+import Dashboard from "../../pages/Dashboard/Dashboard";
+import Bugs from "../../pages/Bugs/Bugs";
 import Login from "../Login/Login";
 import "./app.scss"
 
@@ -11,7 +11,7 @@ function App()
 
   const {token, setToken} = useToken();
 
-  if (!token) return <Login setToken={setToken} />
+  //if (!token) return <Login setToken={setToken} />
 
   return (
     <div className="app">
--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
+import React from 'react';
 import { Link } from "react-router-dom";
+import "./nav.scss";
+import "../../Styles/Globals.scss"
 
 function Nav()
 {
   return (
     <div className="nav">
-      <h2>Issue Tracker</h2>
-      <ul>
+      <div className='logo flex aic'>
+        <i className="fa-solid fa-2x fa-bug-slash logo"></i>
+        <h2 className='color-lightest-gray'>Issue Tracker</h2>
+      </div>
+      <ul >
         <li><Link to="/dashboard">Dashboard</Link></li>
         <li><Link to="/bugs">Bugs</Link></li>
         <li><Link to="/teams">Teams</Link></li>
